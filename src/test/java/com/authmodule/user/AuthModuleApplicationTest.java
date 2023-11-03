e964a1e1ada4509200a2241caa39843ffae13df4
Hongjun Lee <zunza96@gmail.com>
2023-11-03 16:41:47 +0900

    [FEAT]: docker test

--- src/test/java/com/authmodule/user/AuthModuleApplicationTest.java
+++ src/test/java/com/authmodule/user/AuthModuleApplicationTest.java
@@ -1,7 +1,10 @@
 package com.authmodule.user;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.statements.SpringRepeat;
+
 
 public class AuthModuleApplicationTest {
 
