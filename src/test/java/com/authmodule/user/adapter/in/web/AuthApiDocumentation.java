ae90ff7c91e01033ff27694740918d75fbad5cf8
Hongjun Lee <zunza96@gmail.com>
2024-02-23 10:25:48 +0900

    [CHORE]: application-test.yml 위치 변경

--- src/test/java/com/authmodule/user/adapter/in/web/AuthApiDocumentation.java
+++ src/test/java/com/authmodule/user/adapter/in/web/AuthApiDocumentation.java
@@ -17,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -27,7 +28,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
-@SpringBootTest(properties = "spring.config.location=classpath:application-test.yml")
+//@SpringBootTest(properties = "spring.config.location=classpath:application-test.yml")
+@SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 class AuthApiDocumentation {
     @Autowired
